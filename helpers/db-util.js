2a04ddd68fddd4958d89f41b1e8b24ac2395a1cf
abhay <abhaytxanax@gmail.com>
2022-11-02 23:57:20 +0530

    context

--- helpers/db-util.js
+++ helpers/db-util.js
@@ -29,21 +29,13 @@ import { MongoClient } from "mongodb";
 // }
 
 export const connectDatabase = async () => {
-  const client = await MongoClient.connnect(
+  const client = await MongoClient.connect(
     "mongodb+srv://abhay:abhay888@cluster0.kwjblrz.mongodb.net/events?retryWrites=true&w=majority"
   );
-
   return client;
 };
 
-export const getAllDocuments = async (client, collection, sort) => {
+export const insertDocument = async (client, documents) => {
   const db = client.db();
-  const documents = await db.collection(collection).find().sort(sort).toArray();
-  return documents;
-};
-
-export const insertDocument = async (client, collection, document) => {
-  const db = client.db();
-  const result = await db.collection(collection).insertOne(document);
-  return result;
+  await db.collection("newsletter").insertOne(documents);
 };
