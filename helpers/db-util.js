43cfc15af6cd534293a944bb218be4db4439df5b
abhay <abhaytxanax@gmail.com>
2022-11-02 18:42:48 +0530

    db-util-helper

--- helpers/db-util.js
+++ helpers/db-util.js
@@ -1,29 +1,59 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
-export async function connectDatabase() {
-  const client = await MongoClient.connect(
-    'mongodb+srv://maximilian:8ZO3ycZqJ23kWBQx@cluster0.ntrwp.mongodb.net/events?retryWrites=true&w=majority'
+// export async function connectDatabase() {
+//   const client = await MongoClient.connect(
+//     'mongodb+srv://maximilian:8ZO3ycZqJ23kWBQx@cluster0.ntrwp.mongodb.net/events?retryWrites=true&w=majority'
+//   );
+
+//   return client;
+// }
+
+// export async function insertDocument(client, collection, document) {
+//   const db = client.db();
+
+//   const result = await db.collection(collection).insertOne(document);
+
+//   return result;
+// }
+
+// export async function getAllDocuments(client, collection, sort) {
+//   const db = client.db();
+
+//   const documents = await db
+//     .collection(collection)
+//     .find()
+//     .sort(sort)
+//     .toArray();
+
+//   return documents;
+// }
+
+export const connectDatabase = async () => {
+  const client = await MongoClient.connnect(
+    "mongodb+srv://abhay:abhay888@cluster0.kwjblrz.mongodb.net/?retryWrites=true&w=majority"
   );
 
   return client;
-}
+};
 
-export async function insertDocument(client, collection, document) {
-  const db = client.db();
-
-  const result = await db.collection(collection).insertOne(document);
-
-  return result;
-}
-
-export async function getAllDocuments(client, collection, sort) {
-  const db = client.db();
-
+export const getAllDocuments = async (
+  client,
+  collection,
+  sort,
+  filter = {}
+) => {
+  const db = client.db("events");
   const documents = await db
     .collection(collection)
     .find()
     .sort(sort)
+    .filter(filter)
     .toArray();
-
   return documents;
-}
+};
+
+export const insertDocument = async (client, collection, document) => {
+  const db = client.db("events");
+  const result = await db.collection(collection).insertOne(document);
+  return result;
+};
