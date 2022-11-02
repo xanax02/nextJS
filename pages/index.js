5172303ce9eacb816bc43cf312f88ce79962c2cc
abhay <abhaytxanax@gmail.com>
2022-11-03 00:03:53 +0530

    notification-component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import Head from 'next/head';
+import Head from "next/head";
 
-import { getFeaturedEvents } from '../helpers/api-util';
-import EventList from '../components/events/event-list';
-import NewsletterRegistration from '../components/input/newsletter-registration';
+import { getFeaturedEvents } from "../helpers/api-util";
+import EventList from "../components/events/event-list";
+import NewsletterRegistration from "../components/input/newsletter-registration";
 
 function HomePage(props) {
   return (
@@ -10,8 +10,8 @@ function HomePage(props) {
       <Head>
         <title>NextJS Events</title>
         <meta
-          name='description'
-          content='Find a lot of great events that allow you to evolve...'
+          name="description"
+          content="Find a lot of great events that allow you to evolve..."
         />
       </Head>
       <NewsletterRegistration />
